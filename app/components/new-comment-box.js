6a155456f17bee74485a7bc1b932dea019e4f49f
Michael Rashkovsky <mrashkovsky@gmail.com>
2016-01-24 00:03:59 -0500

    Pass form attributes using component state

--- app/components/new-comment-box.js
+++ app/components/new-comment-box.js
@@ -1,9 +1,10 @@
+import Ember from 'ember';
+import $ from 'jquery';
+
 export default Ember.Component.extend({
   actions: {
     submit(){
-      let userName = $(this.element).find('#inputName').val();
-      let commentBody = $(this.element).find('textarea').val();
-      this.get('onSubmit')(userName, commentBody);
+      this.get('onSubmit')(this.name, this.comment);
     }
   }
 });
